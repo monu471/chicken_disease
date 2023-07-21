9eddaecbf722d3a1e700e455a6875096157f90c2
monu471 <monujoshi471@gmail.com>
2023-07-21 19:24:14 +0530

    added basic file

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("Readme.md", encoding="Utf-8") as f:
+    long_description = f.read()
+
+
+__version__ ="0.0.0"
+repo_name = "chicken_disease"
+author = "Monu Joshi"
+src_repo  = "cnnClassifier"
+author_email = "monujoshi471@gmail.com"
+
+
+setuptools.setup(
+    name= src_repo,
+    version=__version__,
+    author= author,
+    author_email= author_email,
+    package_dir= {"":"src"},
+    packages= setuptools.find_packages(where ="src")
+)
